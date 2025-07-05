b281b132765cb82b37fc306396ba849001179373
Lucas-G-Passos <lucasgodpassos@gmail.com>
2025-07-05 13:05:26 -0300

    removed dots

--- src/components/navbar.jsx
+++ src/components/navbar.jsx
@@ -13,7 +13,7 @@ export default function Navbar() {
     <div style={{ display: "flex", height: "100vh" }}>
       <div id="navbar">
         <div id="top-button-cont">
-          <img src="/tigre.png" className="logo" />
+          <img src="/inter.jpg" className="logo" />
           <NavLink to={"/aluno"} className="navlink">
             <div className="navbutton">
               <PeopleIcon />
